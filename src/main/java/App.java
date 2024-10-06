831a30d141543164176c22860c87d1fda02df31b
nstppbl <1nstppbl@gmail.com>
2024-10-06 22:36:16 +0500

    finish task-1
    Took 20 minutes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorldBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("HelloWorld beans are equals? - " + (helloWorldBean1 == helloWorldBean2));
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat beans are equals? = " + (catBean1 == catBean2));
     }
 }
